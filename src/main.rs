7cee0aa15d2cd1d2899360186a86fb16aa3ab1ca
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2024-12-31 01:17:04 -0800

    implementing physics WIP

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,66 @@
+use std::time::Duration;
+
 use avian2d::prelude::*;
 use bevy::{
     prelude::*,
-    reflect::TypeData,
+    reflect::{DynamicTyped, TypeData},
+    time::Stopwatch,
     utils::{HashMap, HashSet},
 };
 use bevy_ecs_ldtk::prelude::*;
 use itertools::Itertools;
+
+const HORIZONTAL_SPEED_PER_SEC: f32 = 80.;
+const JUMP_HEIGHT: f32 = TILE * 3.0;
+const JUMP_HORIZONTAL_DISTANCE: f32 = TILE * 5.;
+const JUMP_BUFFER_SECS: f32 = 0.2;
+// Bevy runs at 64 hz for fixed timestep.
+const HORIZONTAL_SPEED_PER_TICK: f32 = HORIZONTAL_SPEED_PER_SEC / 64.;
+const TILE: f32 = 16.0;
+const HALF: f32 = 1. / 2.;
+const JUMP_INITIAL_VELOCITY: f32 =
+    (2. * JUMP_HEIGHT * HORIZONTAL_SPEED_PER_SEC) / (JUMP_HORIZONTAL_DISTANCE * HALF);
+const GRAVITY: f32 = (2. * JUMP_HEIGHT * HORIZONTAL_SPEED_PER_SEC * HORIZONTAL_SPEED_PER_SEC)
+    / (HALF * HALF * JUMP_HORIZONTAL_DISTANCE * JUMP_HORIZONTAL_DISTANCE);
+const JUMP_DURATION_TO_PEAK_IF_HELD: f32 = (-JUMP_INITIAL_VELOCITY) / -GRAVITY;
+const FAST_FALL_GRAVITY: f32 = GRAVITY * 2.;
+
+const PLAYER_COLLIDER_WIDTH: f32 = 7.;
+const PLAYER_COLLIDER_HEIGHT: f32 = 14.;
+const PLAYER_COLLIDER_ROUNDING: f32 = 2.;
 
 fn main() {
     App::new()
         .add_plugins(PhysicsDebugPlugin::default())
         .add_plugins(PhysicsPlugins::default())
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .insert_resource(Gravity(Vec2::NEG_Y * 100.0))
+        // .insert_resource(Gravity(Vec2::NEG_Y * GRAVITY))
+        .insert_resource(Gravity(Vec2::ZERO))
         .add_plugins(LdtkPlugin)
-        .insert_resource(LevelSelection::index(3))
+        .insert_resource(LevelSelection::index(2))
         .add_systems(Startup, setup)
         .add_systems(Update, setup_wall_physics)
         .add_systems(Update, rescale_colliders)
+        .add_systems(
+            FixedUpdate,
+            (
+                (player_proximity_cast, update_action_buffer),
+                jump,
+                apply_player_gravity,
+            )
+                .chain(),
+        )
         // .add_systems(Update, setup_physics_object_collider)
         .register_ldtk_entity::<PlayerBundle>("Player")
         .register_ldtk_entity::<GoalBundle>("Goal")
         .register_ldtk_int_cell::<WallBundle>(1)
         // .add_systems(Update, draw_tile_gizmo)
+        .add_systems(Update, move_player_from_input)
         // .add_systems(
         //     Update,
         //     (translate_grid_coords_entities, move_player_from_input),
         // )
-        // .add_systems(Update, test)
-        // .add_systems(Update, print_sprite)
+        // .add_systems(Update, test) // .add_systems(Update, print_sprite)
         // .add_systems(Update, gizmos)
         // .add_systems(Update, update_gizmo)
         // .insert_resource(GizmoOrigin(Vec2::ZERO))
@@ -59,27 +91,219 @@ fn main() {
 //     dbg!(hashset);
 // }
 
-// fn move_player_from_input(
-//     mut players: Query<&mut GridCoords, With<Player>>,
-//     input: Res<ButtonInput<KeyCode>>,
-// ) {
-//     let movement_direction = if input.just_pressed(KeyCode::KeyW) {
-//         GridCoords::new(0, 1)
-//     } else if input.just_pressed(KeyCode::KeyA) {
-//         GridCoords::new(-1, 0)
-//     } else if input.just_pressed(KeyCode::KeyS) {
-//         GridCoords::new(0, -1)
-//     } else if input.just_pressed(KeyCode::KeyD) {
-//         GridCoords::new(1, 0)
-//     } else {
-//         return;
-//     };
+#[derive(Debug, Default, strum::EnumIs)]
+enum JumpingStatus {
+    #[default]
+    NotJumping,
+    Jumping {
+        jump_duration: Stopwatch,
+        initial_jump_location: Vec2,
+    },
+    JumpingEarlyRelease {
+        initial_jump_location: Vec2,
+    },
+    // Falling {
+    //     initial_jump_location: Vec2,
+    // },
+}
 
-//     for mut player_grid_coords in players.iter_mut() {
-//         let destination = *player_grid_coords + movement_direction;
-//         *player_grid_coords = destination;
-//     }
-// }
+#[derive(Component, Debug, Default)]
+struct PlayerPhysicsStatus {
+    grounded: bool,
+    left_wall: bool,
+    right_wall: bool,
+    jumping_status: JumpingStatus,
+}
+
+#[derive(Debug, Default)]
+enum ActionBuffer {
+    Buffered(Stopwatch),
+    #[default]
+    None,
+}
+
+impl ActionBuffer {
+    fn reset(&mut self) {
+        *self = ActionBuffer::None;
+    }
+    fn start(&mut self) {
+        *self = ActionBuffer::Buffered(Stopwatch::new());
+    }
+    fn tick(&mut self, dt: Duration) {
+        if let ActionBuffer::Buffered(buffer) = self {
+            buffer.tick(dt);
+        }
+    }
+    fn within_time(&self, secs: f32) -> bool {
+        if let ActionBuffer::Buffered(buffer) = self {
+            return buffer.elapsed_secs() <= secs;
+        }
+        false
+    }
+}
+
+#[derive(Component, Debug, Default)]
+struct ActionBufferStorage {
+    jump: ActionBuffer,
+}
+
+fn apply_player_gravity(
+    time: Res<Time>,
+    mut q: Query<(&mut LinearVelocity, &PlayerPhysicsStatus), With<Player>>,
+) {
+    let Ok((mut player_velocity, status)) = q.get_single_mut() else {
+        return;
+    };
+    match status.jumping_status {
+        JumpingStatus::NotJumping => player_velocity.y -= GRAVITY * time.delta_secs(),
+        JumpingStatus::Jumping { .. } => player_velocity.y -= GRAVITY * time.delta_secs(),
+        JumpingStatus::JumpingEarlyRelease { .. } => {
+            player_velocity.y -= FAST_FALL_GRAVITY * time.delta_secs()
+        }
+    }
+}
+
+fn update_action_buffer(
+    time: Res<Time>,
+    mut q: Query<&mut ActionBufferStorage>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let Ok(mut storage) = q.get_single_mut() else {
+        return;
+    };
+    storage.jump.tick(time.delta());
+    if input.just_pressed(KeyCode::Space) {
+        storage.jump.start();
+    }
+}
+
+fn player_proximity_cast(
+    mut q: Query<(&Transform, &mut PlayerPhysicsStatus)>,
+    spatial_query: SpatialQuery,
+) {
+    let Ok((transform, mut status)) = q.get_single_mut() else {
+        return;
+    };
+    let config = ShapeCastConfig {
+        max_distance: 0.,
+        target_distance: 0.,
+        compute_contact_on_penetration: false,
+        ignore_origin_penetration: false,
+    };
+    let filter = SpatialQueryFilter {
+        ..Default::default()
+    };
+    let down_cast = spatial_query.cast_shape(
+        &Collider::rectangle(PLAYER_COLLIDER_WIDTH - 0.01, 0.),
+        transform.translation.xy() - Vec2::new(0., PLAYER_COLLIDER_HEIGHT / 2. + 0.1),
+        0.,
+        Dir2::NEG_Y,
+        &config,
+        &filter,
+    );
+    let left_cast = spatial_query.cast_shape(
+        &Collider::rectangle(0., PLAYER_COLLIDER_HEIGHT - 0.01),
+        transform.translation.xy() - Vec2::new(PLAYER_COLLIDER_WIDTH / 2. + 0.1, 0.),
+        0.,
+        Dir2::X,
+        &config,
+        &filter,
+    );
+    let right_cast = spatial_query.cast_shape(
+        &Collider::rectangle(0., PLAYER_COLLIDER_HEIGHT - 0.01),
+        transform.translation.xy() + Vec2::new(PLAYER_COLLIDER_WIDTH / 2. + 0.1, 0.),
+        0.,
+        Dir2::NEG_X,
+        &config,
+        &filter,
+    );
+    status.grounded = down_cast.is_some();
+    status.left_wall = left_cast.is_some();
+    status.right_wall = right_cast.is_some();
+}
+
+fn jump(
+    mut player: Query<
+        (
+            &Transform,
+            &mut LinearVelocity,
+            &mut PlayerPhysicsStatus,
+            &mut ActionBufferStorage,
+        ),
+        With<Player>,
+    >,
+    input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let Ok((transform, mut vel, mut status, mut storage)) = player.get_single_mut() else {
+        return;
+    };
+    if status.grounded {
+        status.jumping_status = JumpingStatus::NotJumping;
+    }
+    match status.jumping_status {
+        JumpingStatus::Jumping {
+            ref mut jump_duration,
+            initial_jump_location,
+        } => {
+            jump_duration.tick(time.delta());
+            if jump_duration.elapsed_secs() >= JUMP_DURATION_TO_PEAK_IF_HELD {
+                status.jumping_status = JumpingStatus::NotJumping;
+            }
+            if input.just_released(KeyCode::Space) {
+                status.jumping_status = JumpingStatus::JumpingEarlyRelease {
+                    initial_jump_location,
+                };
+                vel.y = 0.;
+            }
+        }
+        JumpingStatus::JumpingEarlyRelease {
+            initial_jump_location,
+        } => {
+            if initial_jump_location.y >= transform.translation.y {
+                status.jumping_status = JumpingStatus::NotJumping;
+            }
+        }
+        JumpingStatus::NotJumping => {}
+    }
+    if storage.jump.within_time(JUMP_BUFFER_SECS) && status.grounded {
+        storage.jump.reset();
+        vel.y = JUMP_INITIAL_VELOCITY;
+        status.grounded = false;
+        status.jumping_status = JumpingStatus::Jumping {
+            initial_jump_location: transform.translation.xy(),
+            jump_duration: Stopwatch::new(),
+        };
+    }
+}
+
+fn move_player_from_input(
+    mut player: Query<(&mut LinearVelocity, &mut Sprite), With<Player>>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    let Ok((mut velocity, mut sprite)) = player.get_single_mut() else {
+        return;
+    };
+
+    let mut left_right = 0f32;
+    let mut flip = false;
+    if input.pressed(KeyCode::KeyA) {
+        left_right -= HORIZONTAL_SPEED_PER_SEC;
+        flip = true;
+    }
+    if input.pressed(KeyCode::KeyD) {
+        left_right += HORIZONTAL_SPEED_PER_SEC;
+        flip = false;
+    }
+    if left_right == 0. {
+        // if input.any_just_released([KeyCode::KeyA, KeyCode::KeyD]) {
+        velocity.x = 0.;
+        // }
+    } else {
+        sprite.flip_x = flip;
+        velocity.x = left_right;
+    }
+}
 
 // fn setup_physics_object_collider(
 //     mut commands: Commands,
@@ -109,7 +333,6 @@ fn rescale_colliders(
 ) {
     for (entity, mut collider) in query.iter_mut() {
         // collider.set_scale(transform.scale.recip().xy(), 1);
-        dbg!(collider.scale());
         collider.set_scale(Vec2::splat(1.0), 1);
         commands.entity(entity).remove::<PendingRescale>();
     }
@@ -120,7 +343,6 @@ struct PendingRescale;
 
 #[derive(Default, Component)]
 struct Player;
-
 #[derive(Default, Bundle, LdtkEntity)]
 struct PlayerBundle {
     #[sprite_sheet]
@@ -129,18 +351,20 @@ struct PlayerBundle {
     // grid_coords: GridCoords,
     player: Player,
     pending_rescale: PendingRescale,
-    #[with(player_rigid_body)]
+    #[with(|_| RigidBody::Dynamic)]
     rigid_body: RigidBody,
-    #[with(player_collider)]
+    #[with(|_| Collider::round_rectangle(
+        PLAYER_COLLIDER_WIDTH - PLAYER_COLLIDER_ROUNDING * 2.,
+        PLAYER_COLLIDER_HEIGHT - PLAYER_COLLIDER_ROUNDING * 2.,
+        PLAYER_COLLIDER_ROUNDING
+    ))]
     collider: Collider,
-}
-
-fn player_rigid_body(_: &EntityInstance) -> RigidBody {
-    RigidBody::Dynamic
-}
-
-fn player_collider(_: &EntityInstance) -> Collider {
-    Collider::rectangle(16., 16.)
+    #[with(|_| Friction::new(0.).with_combine_rule(CoefficientCombine::Min))]
+    combine: Friction,
+    #[with(|_| LockedAxes::ROTATION_LOCKED)]
+    locked: LockedAxes,
+    proximity: PlayerPhysicsStatus,
+    action_buffers: ActionBufferStorage,
 }
 
 #[derive(Default, Bundle, LdtkEntity)]
@@ -280,8 +504,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Camera2d,
         OrthographicProjection {
-            // scale: 0.5,
-            scale: 3.,
+            scale: 0.5,
             ..OrthographicProjection::default_2d()
         },
         Transform::from_xyz(1280.0 / 4.0, 720.0 / 4.0, 0.0),
